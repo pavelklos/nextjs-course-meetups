7f349864d0b9031ad8f464c14326bf39a9547dc2
pavelklos <pavelkloscz@gmail.com>
2021-04-16 17:49:06 +0200

    249. [06:17] The "_app.js" File & Wrapper Components

--- components/layout/MainNavigation.js
+++ components/layout/MainNavigation.js
@@ -1,17 +1,21 @@
-import classes from './MainNavigation.module.css';
+import Link from "next/link";
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-
   return (
     <header className={classes.header}>
-      <div className={classes.logo}>React Meetups</div>
+      <Link href='/'>
+        <a>
+          <div className={classes.logo}>NextJS Meetups</div>
+        </a>
+      </Link>
       <nav>
         <ul>
           <li>
-            <Link to='/'>All Meetups</Link>
+            <Link href='/'>All Meetups</Link>
           </li>
           <li>
-            <Link to='/new-meetup'>Add New Meetup</Link>
+            <Link href='/new-meetup'>Add New Meetup</Link>
           </li>
         </ul>
       </nav>
