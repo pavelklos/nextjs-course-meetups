3add0625cb404de6de3d5cd5c8c13bfbb727f0ff
pavelklos <pavelkloscz@gmail.com>
2021-04-16 23:55:32 +0200

    251. [10:00] Adding Custom Components & Styling With CSS Modules

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,10 +1,19 @@
 import { useRouter } from "next/router";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetail = () => {
+const MeetupDetails = () => {
   const router = useRouter();
   const meetupId = router.query?.meetupId;
 
-  return <h1>MEETUP DETAIL : {meetupId}</h1>;
+  return (
+    <MeetupDetail
+      image='https://images.pexels.com/photos/2598010/pexels-photo-2598010.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+      title='This is a first meetup'
+      address='Meetupstreet 5, 12345 Meetup City'
+      description='This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!'
+      id='m1'
+    />
+  );
 };
 
-export default MeetupDetail;
+export default MeetupDetails;
